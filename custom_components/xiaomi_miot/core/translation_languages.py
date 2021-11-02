fa70f4cfd035a8d66f69ac842eb5d0235d2c05b1
root <root@core-ssh.local.hass.io>
2021-11-03 01:50:18 +0300

    air purifier fix + some ui upgrades

---
+++ custom_components/xiaomi_miot/core/translation_languages.py
@@ -0,0 +1,115 @@
+TRANSLATION_LANGUAGES = {
+
+    'zh': {
+        'off': '关闭',
+        'idle': '空闲',
+        'busy': '工作中',
+        'pause': '暂停',
+        'fault': '错误',
+
+        '_globals': {
+            'battery level': '电量',
+            'temperature': '温度',
+            'relative humidity': '湿度',
+            'illumination': '光照度',
+            'indicator light': '指示灯',
+        },
+
+        'fan_level': {
+            'auto': '自动',
+            'low': '低档',
+            'medium': '中档',
+            'high': '高档',
+        },
+
+        'mode': {
+            'auto': '自动',
+            'basic': '标准',
+            'low': '低档',
+            'medium': '中档',
+            'high': '高档',
+            'sleep': '睡眠模式',
+            'smart': '智能模式',
+            'favorite': '喜爱模式',
+        },
+
+        'power_consumption': {
+            'power consumption': '用电量',
+            'power consumption electric power': '功率',
+            'power consumption electric current': '电流',
+            'power consumption electric voltage': '电压',
+        },
+
+        'temperature_humidity_sensor': {
+            'temperature humidity sensor temperature': '温度',
+            'temperature humidity sensor relative humidity': '湿度',
+        },
+
+        'air_conditioner.mode': {
+            'cool': '制冷',
+            'dry': '除湿',
+            'fan': '风扇',
+            'heat': '制热',
+        },
+
+        'air_fresh.mode': {
+            'interval': '间歇模式',
+        },
+
+        'fan.mode': {
+            'straight wind': '直吹风',
+            'natural wind': '自然风',
+        },
+
+        'light.mode': {
+            'day': '日光',
+            'night': '夜灯',
+            'color': '彩光',
+        },
+
+        'ptc_bath_heater.mode': {
+            'fan': '吹风',
+            'heat': '暖风',
+            'ventilate': '换气',
+            'dry': '除湿',
+            'defog': '除雾',
+        },
+
+        'vacuum.mode': {
+            'silent': '安静',
+            'strong': '强力',
+        },
+
+        'washer.mode': {
+            'daily wash': '日常洗',
+            'quick wash': '快速洗',
+            'delicate wash': '轻柔洗',
+            'down coat': '羽绒服',
+            'heavy wash': '强力洗',
+            'user define': '自定义',
+            'rinse': '单漂洗',
+            'spin': '单脱水',
+            'cotton': '棉麻洗',
+            'synthetic': '化纤洗',
+            'shirt': '衬衣洗',
+            'boiling': '高温洗',
+            'wool': '羊毛洗',
+            'drum clean': '筒自洁',
+            'baby care': '婴童洗',
+            'intensive': '精细洗',
+            'jacket': '夹克洗',
+            'wash dry': '洗+烘',
+            'underwear': '内衣洗',
+            'dry': '单烘干',
+            'dry air wash': '空气洗',
+            'quick wash dry': '快洗烘',
+        },
+        'washer.drying_level': {
+            'moist': '微湿',
+            'normal': '正常',
+            'extra': '特干',
+            'none': '无烘干',
+        },
+    },
+
+}
